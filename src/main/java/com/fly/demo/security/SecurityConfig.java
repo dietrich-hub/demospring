32b0db46f38b25910d91f1bf314ce81b28455806
dietrich-hub <bonhoefferdietrich3@gmail.com>
2022-03-13 23:57:52 -0700

    security 2

--- src/main/java/com/fly/demo/security/SecurityConfig.java
+++ src/main/java/com/fly/demo/security/SecurityConfig.java
@@ -1,25 +1,49 @@
 package com.fly.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication().withUser("user1").password("1234").roles("USER");
-            auth.inMemoryAuthentication().withUser("user2").password("1234").roles("USER");
-            auth.inMemoryAuthentication().withUser("user3").password("1234").roles("USER","ADMIN");
+
+        PasswordEncoder passwordEncoder=passwordEncoder();
+
+        System.out.println("-----------------------------" + passwordEncoder.encode("1234"));
+           /* auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
+            auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
+            auth.inMemoryAuthentication().withUser("user3").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");*/
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select login as principal,pass as credentials,active from users where login=?")
+                .authoritiesByUsernameQuery("select login as principal,role as role from users_roles where login=?")
+                .passwordEncoder(passwordEncoder)
+                .rolePrefix("ROLE_");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
             http.formLogin();
             //http.httpBasic();
-            http.authorizeRequests().antMatchers("/save**/**","/delete**/**").hasRole("ADMIN");
+            http.authorizeRequests().antMatchers("/save**/**","/delete**/**","/form**/**").hasRole("ADMIN");
             http.authorizeRequests().anyRequest().authenticated();
+            http.exceptionHandling().accessDeniedPage("/notAuthorized");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
